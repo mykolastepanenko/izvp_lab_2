839eac3c8ea3b450c46f70da3fca6aa77ca33f02
Admin <Nikolaua36@gmail.com>
2021-01-08 03:03:09 +0200

    first commit

---
+++ 471 Stepanenko Lab 2/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace _471_Stepanenko_Lab_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(393, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "471 Степаненко М.І. Лаба 2. Варіант 21. Дата: 07.01.2021";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 116);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PlaceholderText = "Координата Х";
+            this.textBox1.Size = new System.Drawing.Size(275, 27);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(201, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Введіть координати масиву";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 149);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PlaceholderText = "Координата У";
+            this.textBox2.Size = new System.Drawing.Size(275, 27);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 181);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(275, 27);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Створити масив";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 214);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 47;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 297);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.Text = "dataGridView1";
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(580, 89);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(153, 24);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Ввод з клавіатури";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(580, 119);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(121, 24);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Ввод з файлу";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(580, 149);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(142, 24);
+            this.radioButton3.TabIndex = 8;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Ввод рандомом";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(580, 179);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 29);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Ввести числа";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(341, 179);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(183, 29);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Виконати інд. з. №21";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 11;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 523);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Лаба 2. Степаненко М.І. 471 варіант 21";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+    }
+}
+
---
+++ 471 Stepanenko Lab 2/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _471_Stepanenko_Lab_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ToolStripMenuItem fileItem = new ToolStripMenuItem("Файл");
+
+            fileItem.DropDownItems.Add("Закрити");
+            fileItem.DropDownItems[0].Click += aboutItem_Click;
+            menuStrip1.Items.Add(fileItem);
+
+            ToolStripMenuItem aboutItem = new ToolStripMenuItem("О программе");
+            aboutItem.DropDownItems.Add("Автор: Степаненко М.І. 471");
+            menuStrip1.Items.Add(aboutItem);
+        }
+
+        private void aboutItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text == "" && textBox2.Text == "" && dataGridView1.Columns.Count == 0)
+            {
+                MessageBox.Show("Ви не ввели X або У, отже було створено масив розміром 2х2.");
+                dataGridView1.Columns.Add("one", "one");
+                dataGridView1.Columns.Add("two", "two");
+                dataGridView1.Rows.Add();
+                dataGridView1.Rows.Add();
+            }
+            else
+            {
+                for (int i = 0; i < Convert.ToInt32(textBox1.Text); i++)
+                {
+                    dataGridView1.Columns.Add("Column " + (i + 1).ToString(), "Column " + (i + 1).ToString());
+                }
+                for (int i = 0; i < Convert.ToInt32(textBox2.Text); i++)
+                {
+                    dataGridView1.Rows.Add("Column " + (i + 1).ToString(), "Column " + (i + 1).ToString());
+                }
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            if(radioButton1.Checked == true)
+            {
+                //ввод з клавіатури
+            }
+            else if (radioButton2.Checked == true)
+            {
+                //ввод з файлу
+            }
+            else if (radioButton3.Checked == true)
+            {
+                //ввод рандомом
+                for (int i = 0; i < dataGridView1.Rows.Count-1; i++) 
+                {
+                    for(int j = 0; j < dataGridView1.Columns.Count; j++)
+                    {
+                        dataGridView1.Rows[i].Cells[j].Value = random.Next(1, 11);
+                    }
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int dob = 1, count = 0;
+            for (int i = 0; i < dataGridView1.Rows.Count - 2; i++)
+            {
+                for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                {
+                    if(j > i)
+                    {
+                        dob *= Convert.ToInt32(dataGridView1.Rows[i].Cells[j].Value);
+                        count++;
+                    }
+                }
+            }
+            MessageBox.Show("Добуток: " + dob.ToString() + "\nКількість: " + count.ToString());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
